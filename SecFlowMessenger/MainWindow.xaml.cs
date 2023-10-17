d3c7b82c8cb2dd13825f460951005285bfa9c922
Dmytro Hranovskyi <wehfis@gmail.com>
2023-10-17 10:59:33 +0300

    recreated a project

--- SecFlowMessenger/MainWindow.xaml.cs
+++ SecFlowMessenger/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace SecFlow
+namespace SecFlowMessenger
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
