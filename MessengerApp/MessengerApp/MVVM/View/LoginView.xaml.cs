3e75f9bc75d172dff9551d72c7b41f665352cf5e
Andrii Savka <andrii2353@gmail.com>
2023-11-11 13:56:15 +0200

    start making login page

---
+++ MessengerApp/MessengerApp/MVVM/View/LoginView.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace MessengerApp.MVVM.View
+{
+    /// <summary>
+    /// Interaction logic for Login.xaml
+    /// </summary>
+    public partial class Login : Window
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                DragMove();
+            }
+        }
+    }
+}
