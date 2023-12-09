ee88af7d3bae65373a35b7cf50cf2be20618e0df
Dmytro Hranovskyi <wehfis@gmail.com>
2023-12-09 20:31:06 +0200

    Added repos

---
+++ MessangerServer/DAL/Persistence/Repositories/DialogRepository.cs
@@ -0,0 +1,18 @@
+﻿using BLL.Core.Domain;
+using BLL.Core.Repositories;
+using DAL.DBContex;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Persistence.Repositories
+{
+    internal class DialogRepository : Repository<Dialog>, IDialogRepository
+    {
+        public DialogRepository(DbAplicationContext context) : base(context)
+        {
+        }
+    }
+}
---
+++ MessangerServer/DAL/Persistence/Repositories/MessageRepository.cs
@@ -0,0 +1,18 @@
+﻿using BLL.Core.Domain;
+using BLL.Core.Repositories;
+using DAL.DBContex;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Persistence.Repositories
+{
+    internal class MessageRepository : Repository<Message>, IMessageRepository
+    {
+        public MessageRepository(DbAplicationContext context) : base(context)
+        {
+        }
+    }
+}
