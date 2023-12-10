b2e22b90e0c92c63e98c77741bb3c7e7deb081a6
Dmytro Hranovskyi <wehfis@gmail.com>
2023-12-10 20:49:42 +0200

    add data request handler

--- MessangerServer/MessangerServer/SocketLogic/ServerEventsHandlers/DataRequestHandler.cs
+++ MessangerServer/MessangerServer/SocketLogic/ServerEventsHandlers/DataRequestHandler.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using BLL.Core.Domain;
+using BLL.Core.Repositories;
+using DAL.DBContex;
+using DAL.Persistence.Repositories;
+using MessangerServer.SocketLogic.SocketEventsGenerators;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +11,29 @@ using System.Threading.Tasks;
 
 namespace MessangerServer.SocketLogic.ServerEventsHandlers
 {
-    internal class DataRequestHandler
+    public static class DataRequestHandler
     {
+
+        public static void HandleGetAllUsersExceptCurrent(Event eventParam)
+        {
+            DbAplicationContext Context = new DbAplicationContext();
+            IUserRepository userRepository = new UserRepository(Context);
+
+            string email = eventParam.Parameters["email"].ToString();
+            string password = eventParam.Parameters["password"].ToString();
+
+            var tryGetUser = userRepository.Find(user => user.Email != email );
+            if (tryGetUser == null)
+            {
+                var message = "No users found";
+                var response = ResponseGenerator.GenerateErrorResponse(EventType.Error, message);
+                SocketInitializer.serverSocketManager.SendEvent(response);
+            }
+            else
+            {
+                var sucessResponseEvent = ResponseGenerator.GetAllUsersExceptCurrentResponse(EventType.GetAllUsersExceptCurrent, tryGetUser);
+                SocketInitializer.serverSocketManager.SendEvent(sucessResponseEvent);
+            }
+        }
     }
 }
